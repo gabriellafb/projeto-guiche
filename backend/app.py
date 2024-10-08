33ef18c247253cf95ac6377c0429dd781eb1ea83
VitorioSilva <vitoriodev@proton.me>
2024-10-08 03:33:58 -0300

    Adicionei funcionalidade de login ao sistema

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,10 @@ app = Flask(__name__, static_folder='../frontend/static',
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
