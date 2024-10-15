ef4137990e968d31f14cf21338d5b76b11d5ea8c
Caua Nascimento <txcaua0py@gmail.com>
2024-10-14 22:37:17 -0300

    Duas tabelas criadas

---
+++ models/banco_de_dados.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE cliente(
+	id_cliente int not null PRIMARY KEY AUTO_INCREMENT
+	nome_cliente varchar(50)
+	sobrenome_cliente varchar(50)
+	email_cliente varchar(255)
+	
+);
+
+
+CREATE TABLE servicos (
+    id_servico INT PRIMARY KEY AUTO_INCREMENT,
+    nome_servico VARCHAR(100) NOT NULL,
+    descricao VARCHAR(255),
+    preço DECIMAL(10, 2) NOT NULL,
+    duração_estimativa INT, -- em minutos
+    disponibilidade BOOLEAN DEFAULT TRUE
+);
+
